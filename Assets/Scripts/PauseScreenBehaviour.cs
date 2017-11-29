83e7e77aac8eb7cd7c2f9488758b1c594c3e01ed
hk021 <harshalk@packtpub.com>
2017-11-29 16:40:36 +0530

    Code files

---
+++ Assets/Scripts/PauseScreenBehaviour.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // SceneManager 
+
+public class PauseScreenBehaviour : MainMenuBehaviour
+{
+
+    public static bool paused;
+
+    [Tooltip("Reference to the pause menu object to turn on/off")]
+    public GameObject pauseMenu;
+
+    /// <summary> 
+    /// Reloads our current level, effectively "restarting" the     
+    /// game 
+    /// </summary> 
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    /// <summary> 
+    /// Will turn our pause menu on or off 
+    /// </summary> 
+    /// <param name="isPaused"></param> 
+    public void SetPauseMenu(bool isPaused)
+    {
+        paused = isPaused;
+
+        // If the game is paused, timeScale is 0, otherwise 1 
+        Time.timeScale = (paused) ? 0 : 1;
+
+        if (paused)
+        {
+            SlideMenuIn(pauseMenu);
+        }
+        else
+        {
+            SlideMenuOut(pauseMenu);
+        }
+    }
+
+    protected override void Start()
+    {
+        // Initalize Ads if needed
+        base.Start();
+
+        paused = false;
+
+        // If no ads at all, just unpause 
+        #if !UNITY_ADS 
+            SetPauseMenu(false); 
+        #else 
+ 
+        // If we support ads but they're removed, unpause as well 
+        if (!UnityAdController.showAds) 
+        { 
+            SetPauseMenu(false); 
+        } 
+ 
+        #endif 
+    }
+
+    #region Share Score via Twitter 
+
+    // Web address in order to create a tweet 
+    private const string tweetTextAddress =
+                                "http://twitter.com/intent/tweet?text=";
+
+    // Where we want players to visit 
+    private string appStoreLink = "http://johnpdoran.com/";
+
+    // Reference to the player for the score 
+    public PlayerBehaviour player;
+
+    /// <summary> 
+    /// Will open Twitter with a prebuilt tweet. When called on iOS or  
+    /// Android will open up Twitter app if installed 
+    /// </summary> 
+    public void TweetScore()
+    {
+        // Get contents of the tweet (in URL friendly format) 
+        string tweet = "I got " + string.Format("{0:0}", player.Score)
+    + " points in Endless Roller! Can you do better?";
+
+        // Open the URL to create the tweet 
+        Application.OpenURL(tweetTextAddress + WWW.EscapeURL(tweet +
+                            "\n" + appStoreLink));
+    }
+
+    #endregion
+
+}
---
+++ Assets/Scripts/UnityAdController.cs
@@ -0,0 +1,100 @@
+﻿using System; // DateTime
+using UnityEngine;
+
+#if UNITY_ADS // Can only compile ad code on support platforms 
+
+using UnityEngine.Advertisements; // Advertisement 
+
+#endif
+
+public class UnityAdController : MonoBehaviour
+{
+    // If we should show ads or not
+    public static bool showAds = true;
+
+    // Nullable type 
+    public static DateTime? nextRewardTime = null;
+
+
+    public static void ShowAd()
+    {
+
+#if UNITY_ADS
+
+        // Set options for our advertisement 
+        ShowOptions options = new ShowOptions();
+        options.resultCallback = Unpause;
+
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show(options);
+        }
+
+        // Pause game while ad is shown 
+        PauseScreenBehaviour.paused = true;
+        Time.timeScale = 0f;
+
+#endif
+
+    }
+
+#if UNITY_ADS
+
+    public static void Unpause(ShowResult result)
+    {
+        // Unpause when ad is over 
+        PauseScreenBehaviour.paused = false;
+        Time.timeScale = 1f;
+    }
+
+#endif
+
+
+    public static void ShowRewardAd()
+    {
+#if UNITY_ADS
+
+        nextRewardTime = DateTime.Now.AddSeconds(15);
+
+        if (Advertisement.IsReady())
+        {
+            // Pause game while ad is shown 
+            PauseScreenBehaviour.paused = true;
+            Time.timeScale = 0f;
+
+            var options = new ShowOptions { resultCallback = HandleShowResult };
+            Advertisement.Show(options);
+        }
+
+#endif
+    }
+
+    // For holding the obstacle for continuing the game 
+    public static ObstacleBehaviour obstacle;
+
+    private static void HandleShowResult(ShowResult result)
+    {
+#if UNITY_ADS
+
+        switch (result)
+        {
+            case ShowResult.Finished:
+                // Successfully shown, can continue game 
+                obstacle.Continue();
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("Ad skipped, do nothing");
+                break;
+            case ShowResult.Failed:
+                Debug.LogError("Ad failed to show, do nothing");
+                break;
+        }
+
+#endif
+        // Unpause when ad is over or when called
+        PauseScreenBehaviour.paused = false;
+        Time.timeScale = 1f;
+
+    }
+
+}
